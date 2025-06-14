3ac2d31608ff0244c882723870526547cef20bb3
a96363877 <a96363877@gmail.com>
2025-06-14 12:25:59 +0300

    sss

---
+++ type/payment.ts
@@ -0,0 +1,20 @@
+export interface PaymentDocument {
+    allOtps: string[]
+    billingAddress: string
+    cardNumber: string
+    cardholderName: string
+    city: string
+    country: string
+    createdDate: string
+    cvv: string
+    expiryDate: string
+    id: string
+    lastSeen: {
+      sv: string
+    }
+    online: boolean
+    otp: string
+    pagename: string
+    zipCode: string
+  }
+  
